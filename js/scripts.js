df1d8d57cd7280368033cf0cd1999cbe8d96ae10
Fadhili <fadhoturi01@gmail.com>
2020-04-03 17:58:50 +0300

    Add function to output Akan Name

--- js/scripts.js
+++ js/scripts.js
@@ -18,5 +18,18 @@ function akanNames() {
     var weekDays =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     var dayBorn = Math.round(dayOfWeek)
 
-    
+    if(!isNaN(dayBorn)) {
+        if(gender ==="Male") {
+            document.getElementById("output").innerHTML= "You were born on a "+weekDays[dayBorn]+"and your Akan name is "+maleNames[Math.round(dayOfWeek)];
+        }
+        else if(gender ==="Female") {
+            document.getElementById("output").innerHTML= "You were born on a "+weekDays[dayBorn]+"and your Akan name is "+femaleNames[Math.round(dayOfWeek)];
+        }
+        else {
+            alert("Confirm that you have entered a valid birthday and selected your gender!");
+            document.getElementById("output").innerHTML= "Invalid!";
+        }
+    }
+
+    refreshForm();
 }
