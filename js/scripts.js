7f5905ad5491293efe99b2e27bf5159f1c0a4745
Fadhili <fadhoturi01@gmail.com>
2020-04-03 17:38:14 +0300

    Add variables used to calculate day of week from a certain date

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
 function refreshForm() {
     document.getElementById("user_input").reset()
 }
+
+function akanNames() {
+    var birthDay = document.getElementById("birthdate").value;
+    var enteredDate = birthDay.split("-");
+    var day = parseInt(enteredDate[2]);
+    var month = parseInt(enteredDate[1]);
+    var year = parseInt(enteredDate[0]);
+    var century = (((year-1)/100)+1);
+    var dayOfWeek = (((century/4)-2*century-1)+(5*(year/4))+(26*(month+1)/10)+day)%7;
+
+    document.getElementById("output").innerHTML= Math.round(dayOfWeek);
+    
+}
